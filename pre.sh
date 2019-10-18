ed797be264a187256960f5c2bdb4b0ae18a53e3b
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-10-18 11:28:02 +0800

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,1 +1,2 @@
-npm set registry http://8081.gr51c1b2.iyl0r2oh.0196bd.grapps.cn/repository/npm-group/
+#npm set registry http://8081.gr51c1b2.iyl0r2oh.0196bd.grapps.cn/repository/npm-group/
+npm config set registry=http://8081.gr51c1b2.iyl0r2oh.0196bd.grapps.cn/repository/npm-group/
