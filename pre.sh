35189631a0e3b68911f8e8b1f5785b7f2ec83567
Guox <33855684+dazuimao1990@users.noreply.github.com>
2019-10-18 11:47:31 +0800

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,2 +1,3 @@
 #npm set registry http://8081.gr51c1b2.iyl0r2oh.0196bd.grapps.cn/repository/npm-group/
 npm config set registry=http://8081.gr51c1b2.iyl0r2oh.0196bd.grapps.cn/repository/npm-group/ --global
+npm config get cache
